7e64848116b0cafd9e269896e8b57c966a52eedd
Александр Гвоздецкий <hvozdetskyi.alex@gmail.com>
2021-09-19 14:55:32 +0300

    +

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,15 +1,12 @@
 import refs from "./refs";
-console.log(refs.arrowUp)
 const arrHidden = document.querySelectorAll('.hidden')
-const container = refs.accountContainer.classList;
 const sidebar = refs.sideBar.classList;
 const edit = refs.edit.classList;
-console.log(refs.accountContainer)
 refs.arrowUp.addEventListener('click', onToggle)
 function onToggle() {
     arrHidden.forEach(elem => {
         elem.classList.toggle('hidden')
     })
-    sidebar.toggle('high')
-    edit.toggle('move')
+    refs.sideBar.classList.toggle('high')
+    refs.edit.classList.toggle('move')
 }
--- src/js/crossClose.js
+++ src/js/crossClose.js
@@ -2,6 +2,5 @@ import refs from './refs';
 
 refs.cross.addEventListener('click', onHide, { once: true })
 function onHide(e) {
-    console.log('Hide')
     refs.notification.style.display='none'
 }
